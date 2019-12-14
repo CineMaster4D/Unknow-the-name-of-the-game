294e87fc3f16fad76cda2022e2416032fb98ddfc
wingrada@gmail.com <wingrada@gmail.com>
2019-12-14 20:19:30 +0500

    Я создал три класса character, person, bot. В классе Character я создал свойства Nickname и HealthPoint. Так же в этом классе я создал метод Display который выводит имя и жизни.

---
+++ FurtureGame/FurtureGame/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurtureGame
+{
+    class Character
+    {
+        public string Nickname;
+        public int HealthPoint;
+
+        public void Display(string Name, int Health)
+        {
+            Console.WriteLine(Name + " " + Health);
+        }
+    }
+}
---
+++ FurtureGame/FurtureGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurtureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Bot bot = new Bot();
+
+            person.Nickname = "Anatola";
+            person.HealthPoint = 100;
+
+            bot.Nickname = "Wolf";
+            bot.HealthPoint = 30;
+
+            person.Display(person.Nickname, person.HealthPoint);
+            bot.Display(bot.Nickname, bot.HealthPoint);
+            Console.ReadLine();
+        }
+    }
+}
